9ebf047534cd1e6a6fbe2d1cd98c851a972597c1
ketanrd26 <ketandudka566123@gmail.com>
2024-12-02 22:19:59 +0530

    add vercel

--- database/db.js
+++ database/db.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const dbConnection = mysql.createPool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
+    host:process.env.HOST ,
+    user:process.env.USER ,
+    password:process.env.PASSWORD , 
     database:process.env.DB_NAME
 });
 
