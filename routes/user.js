7cd5dedb7f459e89f44269bbabc085977747ecb1
Ketan Dudka <ketandudka566123@gmail.com>
2024-12-09 14:39:29 +0530

    poush

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,4 @@ import { loginuser, registeruser } from "../controller/userController.js";
 export const userRoute = express.Router();
 
 userRoute.post("/register", registeruser);
-userRoute.post("/login", loginuser)
+userRoute.post("/login", loginuser);
