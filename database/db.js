8c0eeaaf26aaa460ab57b08ba58e14ac7a92f0d9
Ketan Dudka <ketandudka566123@gmail.com>
2025-01-03 18:21:49 +0530

    push

--- database/db.js
+++ database/db.js
@@ -4,12 +4,18 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const dbConnection = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB_NAME,
+    host: "103.191.209.63",
+    user: "quqdaabl_worker",
+    password: "mydatabaseworker30_30",
+    database: "quqdaabl_worker",
     connectionLimit: 75,
     connectTimeout: 60000,
+    // host: process.env.HOST,
+    // user: process.env.USER,
+    // password: process.env.PASSWORD,
+    // database: process.env.DB_NAME,
+    // connectionLimit: 75,
+    // connectTimeout: 60000,
     // ssl: {
     //   rejectUnauthorized: true, // or false, based on your requirements
     // },
