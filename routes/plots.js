be5bccdc023a9a886ac9aacbde6937f0b7c2e13a
Ketanrd26 <ketandudka566123@gmail.com>
2025-02-06 04:54:31 -0800

    push

--- routes/plots.js
+++ routes/plots.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { allPlotsDetails, getAllPlots, getAvailabalePlot, getAvailabalePlotByprojctId, getPlotById,  getPlotsByProjectId,  plotadd } from "../controller/plotController.js";
+import { allPlotsDetails, getAllPlots, getAvailabalePlot, getAvailabalePlotByprojctId, getPlotById,  getPlotsByProjectId,  getSellPlotsByProjectId,  plotadd } from "../controller/plotController.js";
 
 export const plotRoute = express.Router();
 
@@ -10,3 +10,4 @@ plotRoute.get("/getPlotById/:plotId", getPlotById);
 plotRoute.get("/getAllLength", allPlotsDetails); 
 plotRoute.get("/getPlotsByProjectId/:projectId", getPlotsByProjectId); 
 plotRoute.get("/getAvailablePlots/:projectId", getAvailabalePlotByprojctId); 
+plotRoute.get("/getSellPlotsByProjectId/:projectId", getSellPlotsByProjectId); 
